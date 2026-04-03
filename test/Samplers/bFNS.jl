6813c5a1fe6cf98b875e5e824d86a303726af820
brendanjohnharris <brendanjohnharris@gmail.com>
2026-04-03 16:40:46 +1100

    Up bFNS script

--- test/Samplers/bFNS.jl
+++ test/Samplers/bFNS.jl
@@ -1,4 +1,5 @@
 using FractionalNeuralSampling
+using Random
 using StochasticDiffEq
 using Distributions
 using TimeseriesTools
@@ -12,6 +13,7 @@ Foresight.set_theme!(Foresight.foresight(:physics))
 import FractionalNeuralSampling: Density
 
 begin
+    Random.seed!(11)
     α = 1.4
     β = 1.0
     γ = 0.0
@@ -21,15 +23,28 @@ begin
     domain = -10 .. 10
     boundaries = PeriodicBox(-5 .. 5)
     u0 = [0.0, 0.0]
-    tspan = 5000.00
+    tspan = 10000.00
 
     S = bFNS(; α, β, γ, η, u0, 𝜋, dt, tspan, boundaries, domain)
     sol = solve(S, EM(); dt) |> Timeseries |> eachcol |> first
     sol = rectify(sol, dims = 𝑡; tol = 1)
-    hist(sol; normalization = :pdf, bins = -3:0.1:3)
-    lines!(-3:0.1:3, 𝜋.(-3:0.1:3), color = :red, linewidth = 2)
-    current_figure() |> display
+end
 
+begin # * Plot
+    f = FourPanel()
+
+    ax = Axis(f[1, 1:2], xlabel = "t", ylabel = "x(t)")
+    lines!(ax, sol[1:5000]) # Plot trajectory
+
+    # Plot histogram
+    ax = Axis(f[2, 1], xlabel = "x", ylabel = "Density")
+    hist!(ax, sol; normalization = :pdf, bins = -3:0.1:3)
+    lines!(ax, -3:0.1:3, 𝜋.(-3:0.1:3), color = :red, linewidth = 2)
+
+    # Plot sampling accuracy
+    ax = Axis(f[2, 2], xlabel = "t", ylabel = "Accuracy")
     ws = samplingaccuracy(sol, 𝜋, 1000:1000:10000; p = 100, domain = domain)
-    lines(mean.(ws))
+    lines!(ax, mean.(ws))
+
+    f |> display
 end
